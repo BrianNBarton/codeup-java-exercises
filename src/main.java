c7d0bff2070dcbd14260d031c1f3f8f2843b2922
Brian Barton <brian.n.barton@gmail.com>
2020-08-07 17:04:42 -0500

    Finished Java

---
+++ src/main.java
@@ -0,0 +1,45 @@
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+import java.net.SocketOption;
+import java.net.SocketPermission;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("hello, world!");
+
+//
+//
+//        int Favorite_Number = 27;
+//        System.out.println(Favorite_Number);
+//
+//        long myNumber = 3;
+//
+//
+//      String MyString = "Look at me coding Java";
+//    System.out.println(MyString);
+//
+//    char FirstCharacter = 'B';
+//    System.out.println(FirstCharacter);
+//
+//    long MyNumber = ;
+//    System.out.println(MyNumber);
+
+
+        int x = 7;
+        System.out.println(x++);
+        System.out.println(x);
+        System.out.println(++x);
+
+//
+//        String Class = "things";
+//
+//
+
+
+        long money = Integer.MAX_VALUE;
+       System.out.println(money);
+
+//
+//        int three = (int) "three";
+    }
+}
+
