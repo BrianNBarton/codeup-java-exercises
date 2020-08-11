54f531c8b1c5eb6948ce624e6ec60e31f463c21b
Brian Barton <brian.n.barton@gmail.com>
2020-08-11 15:52:14 -0500

    console exercises

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -1,57 +1,58 @@
 //import java.util.Scanner;
 //
 //public class ConsoleExercises {
-//public static void main(String[] args) {
 //
-//    double pi = 3.14159;
+//    public static void main(String args[]) {
 //
-//    System.out.printf("look two digits of pi %.2f.\n\n", pi);
+//        // Write some Java code that uses the variable pi to output the following:
+//        // The value of pi is approximately 3.14.
+//        double pi = 3.14159;
+//        System.out.printf("\nThe value of pi is approximately %.2f.\n\n", pi);
 //
-//    Scanner scan = new Scanner(System.in);
-//
-//
-//    System.out.print("Please enter 3 words: ");
-//    String str0 = scan.next();
-//    String str1 = scan.next();
-//    String str2 = scan.next();
-//    System.out.println(String.format("str0: %s str1: %s str2: %s\n",str0, str1, str2));
-//    scan.nextLine();
-//
-//    System.out.println("enter a sentence: ");
-//    String sentence = scan.nextLine();
-//    System.out.printf("the user entered: \"%s\"", sentence);
-//
-//
-//
-//    }
-//
-//
-//}
-//
-//public class AreaOfRectangle {
-//    public static void main (String[] args)
-//    {
+//        // Prompt a user to enter a integer and store that value in an int variable using the nextInt method
+//        //What happens if you input something that is not an integer? (java.util.InputMismatchException)
 //        Scanner scanner = new Scanner(System.in);
-//        System.out.println("Enter the length of Rectangle:");
-//        double length = scanner.nextDouble();
-//        System.out.println("Enter the width of Rectangle:");
-//        double width = scanner.nextDouble();
-//        //Area = length*width;
-//        double area = length*width;
-//        System.out.println("Area of Rectangle is:"+area);
-//        double perimeter = length*2 + width*2;
-//        System.out.println("perimeter is "+perimeter);
+////        scanner.useDelimiter("\n");
+//        System.out.print("Enter an integer: ");
+//        int i = scanner.nextInt();
+//        System.out.println("You entered: --> \"" + i + "\" <--");
 //
-//    }
-//}
 //
-//class ControlFlowExercise {
-//    public static void  main (String[] args)
-//    {
-//        int i = 0;
-//        while (i < 15) {
-//            System.out.println(i);
-//            i++;
-//        }
+//        // Prompt a user to enter 3 words, and store each of them in a separate variable.
+//        // Then, display them back in the console, each on a newline.
+//        //What happens if you enter less than 3 words?
+//        //What happens if you enter more than 3 words?
+//        System.out.print("\nEnter 3 words: ");
+//        String word01, word02, word03;
+//        word01 = scanner.next();
+//        word02 = scanner.next();
+//        word03 = scanner.next();
+//        System.out.printf("\n the words are \"%s\", \"%s\", and \"%s\".\n", word01, word02, word03);
+//        scanner.nextLine();
+//
+//        // Prompt a user to enter a sentence, then store that sentence in a String variable using the next method.
+//        // Then, display that sentence back to the user.
+//        System.out.print("\nEnter a sentence: ");
+//        scanner.useDelimiter("\n");
+//        String sentence = scanner.next();
+//        scanner.nextLine();
+//        System.out.printf("\nThe sentence is \"%s\"\n", sentence);
+//
+//        // Rewrite the above example using the nextLine method.
+//        System.out.print("\nEnter a sentence: ");
+//        sentence = scanner.nextLine();
+//        System.out.printf("\nThe sentence is \"%s\"\n", sentence);
+//
+//        // Calculate the perimeter and area of Codeup's classrooms.
+//        Scanner scan = new Scanner(System.in);
+//        System.out.print("\nEnter the width: ");
+//        String sWidth = scan.nextLine();
+//        double width = Double.parseDouble(sWidth);
+//        System.out.print("Enter the length: ");
+//        String sLength = scan.nextLine();
+//        double length = Double.parseDouble(sLength);
+//        System.out.printf("The dimensions are %.2f feet by %.2f feet. The perimeter is %.2f feet and the area is %.2f sq. ft.\n",
+//                length, width, 2*(length+width), length*width);
+//
 //    }
 //}
