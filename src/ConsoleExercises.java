0732423854762602a60ad1ce0d2c4a1689b0ce1a
Brian Barton <brian.n.barton@gmail.com>
2020-08-11 10:32:02 -0500

    control flow

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -1,43 +1,57 @@
-import java.util.Scanner;
-
-public class ConsoleExercises {
-public static void main(String[] args) {
-
-    double pi = 3.14159;
-
-    System.out.printf("look two digits of pi %.2f.\n\n", pi);
-
-    Scanner scan = new Scanner(System.in);
-
-
-    System.out.print("Please enter 3 words: ");
-    String str0 = scan.next();
-    String str1 = scan.next();
-    String str2 = scan.next();
-    System.out.println(String.format("str0: %s str1: %s str2: %s\n",str0, str1, str2));
-
-
-
-
-
-    }
-
-
-}
-
-class AreaOfRectangle {
-    public static void main (String[] args)
-    {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the length of Rectangle:");
-        double length = scanner.nextDouble();
-        System.out.println("Enter the width of Rectangle:");
-        double width = scanner.nextDouble();
-        //Area = length*width;
-        double area = length*width;
-        System.out.println("Area of Rectangle is:"+area);
-        double perimeter = length*2 + width*2;
-        System.out.println("perimeter is "+perimeter);
-
-    }
-}
+//import java.util.Scanner;
+//
+//public class ConsoleExercises {
+//public static void main(String[] args) {
+//
+//    double pi = 3.14159;
+//
+//    System.out.printf("look two digits of pi %.2f.\n\n", pi);
+//
+//    Scanner scan = new Scanner(System.in);
+//
+//
+//    System.out.print("Please enter 3 words: ");
+//    String str0 = scan.next();
+//    String str1 = scan.next();
+//    String str2 = scan.next();
+//    System.out.println(String.format("str0: %s str1: %s str2: %s\n",str0, str1, str2));
+//    scan.nextLine();
+//
+//    System.out.println("enter a sentence: ");
+//    String sentence = scan.nextLine();
+//    System.out.printf("the user entered: \"%s\"", sentence);
+//
+//
+//
+//    }
+//
+//
+//}
+//
+//public class AreaOfRectangle {
+//    public static void main (String[] args)
+//    {
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Enter the length of Rectangle:");
+//        double length = scanner.nextDouble();
+//        System.out.println("Enter the width of Rectangle:");
+//        double width = scanner.nextDouble();
+//        //Area = length*width;
+//        double area = length*width;
+//        System.out.println("Area of Rectangle is:"+area);
+//        double perimeter = length*2 + width*2;
+//        System.out.println("perimeter is "+perimeter);
+//
+//    }
+//}
+//
+//class ControlFlowExercise {
+//    public static void  main (String[] args)
+//    {
+//        int i = 0;
+//        while (i < 15) {
+//            System.out.println(i);
+//            i++;
+//        }
+//    }
+//}
