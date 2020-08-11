70e7477b7a7e5c2b32b9aad50e716d3287ca43c7
Brian Barton <brian.n.barton@gmail.com>
2020-08-11 12:13:37 -0500

    string exercise part1

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -1,9 +1,30 @@
 import org.w3c.dom.ls.LSOutput;
 
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class StringExercise {
+    public static void main(String[] args) {
 
 
+        String PinkFloyd =
+                "We don't need no education\n" +
+                        "We don't need no thought control";
+
+
+        System.out.println(PinkFloyd);
+
+
+        String NewString = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(NewString);
+
+        String ThisOtherString = "In windows, the main drive is usually C:\"";
+        System.out.println(ThisOtherString);
+
+        String YetAnotherString = "I can do backslashes \\, double backslashes \\\\,\n" +
+                "and the amazing triple backslash \\\\\\!";
+        System.out.println(YetAnotherString);
+
+    }
 
 }
