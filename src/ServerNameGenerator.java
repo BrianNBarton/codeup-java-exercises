d1ca75785c0de5e18d740724c603fbd3bed4b54c
Brian Barton <brian.n.barton@gmail.com>
2020-08-16 10:53:33 -0500

    Server Name generator

---
+++ src/ServerNameGenerator.java
@@ -0,0 +1,29 @@
+import java.util.Random;
+import java.util.Scanner;
+public class ServerNameGenerator {
+
+
+    public static void main(String arg[]){
+        //Object to get user input
+        Scanner in = new Scanner(System.in);
+
+        //String array
+        String[] Adjectives = { "Crunchy", "Spicy", "Humid", "Happy", "Focused", "Shaggy", "Silly", "Tenacious", "Fast", "Old"};
+
+        String[] Nouns = { "Dog", "Cat", "Ferret", "Birb", "Turtle", "Chameleon", "Fern", "Orchid", "Carrot", "Chips" } ;
+
+        System.out.print("How many Server Names do you want: ");
+        //Get user input
+        int numberOfTest = in.nextInt();
+
+        for(int i = 0; i < numberOfTest; i++){
+            int index = (int)(Math.random() * 10);
+            System.out.println(Adjectives[index] + " " + Nouns[index]);
+        }
+    }
+
+
+}
+//    public String[] Adjectives = { "Crunchy", "Spicy", "Humid", "Happy", "Focused", "Shaggy", "Muffled", "Hollow", "Fast", "Old"};
+//
+//    public String[] Nouns = { "Dog", "Cat", "Ferret", "Birb", "Turtle", "Chameleon", "Fern", "Orchid", "Carrot", "Chips" } ;
