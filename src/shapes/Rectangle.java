e6c17a7d3c6948f2711dd51099799d708a652629
Brian Barton <brian.n.barton@gmail.com>
2020-08-17 19:03:31 -0500

    more shapes

---
+++ src/shapes/Rectangle.java
@@ -0,0 +1,40 @@
+package shapes;
+
+
+public class Rectangle {
+
+    protected double length;
+    protected double width;
+
+
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+
+    }
+
+
+//    public double getPerimeter(double length, double width) {
+//        return 2 * length + 2 * width;
+//
+//    }
+
+    public static void main(String[] args) {
+
+    }
+
+//    public double getArea(double length, double width) {
+//        return length * width;
+//    }
+
+
+    public double getArea() {
+        return length * width;
+    }
+
+    public double getPerimeter(double length, double width) {
+        return 2 * length + 2 * width;
+    }
+
+}
+
