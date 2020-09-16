018796aa11ed792bc3027d57e6b941b56363cfa8
Brian Barton <brian.n.barton@gmail.com>
2020-09-16 09:10:00 -0600

    three sum

--- src/MorningWarmups.java
+++ src/MorningWarmups.java
@@ -1,7 +1,5 @@
 import java.awt.image.ImageProducer;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class MorningWarmups {
 //this Class is for writing at least one method a day.
@@ -28,10 +26,50 @@ public static void main(String[] args) {
                 map.put(target-nums[i], i);
             }
         }
-
+        for (int i = 0; i < ; i++) {
+            
+        }
         return new int[]{0,0};
     }
 //
+
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+
+        ArrayList<List<Integer>> result = new ArrayList<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int j = i + 1;
+            int k = nums.length - 1;
+
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            while (j < k) {
+                if (k < nums.length - 1 && nums[k] == nums[k + 1]) {
+                    k--;
+                    continue;
+                }
+
+                if (nums[i] + nums[j] + nums[k] > 0) {
+                    k--;
+                } else if (nums[i] + nums[j] + nums[k] < 0) {
+                    j++;
+                } else {
+                    ArrayList<Integer> l = new ArrayList<>();
+                    l.add(nums[i]);
+                    l.add(nums[j]);
+                    l.add(nums[k]);
+                    result.add(l);
+                    j++;
+                    k--;
+                }
+            }
+        }
+
+        return result;
+    }
 //    We have two special characters.
 //    The first character can be represented by one bit 0.
 //    The second character can be represented by two bits (10 or 11).
