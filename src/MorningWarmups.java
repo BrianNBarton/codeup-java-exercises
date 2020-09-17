122955e0d483d628ca9f412e3c8fe195b31e061e
Brian Barton <brian.n.barton@gmail.com>
2020-09-17 07:47:09 -0600

    straight line check

--- src/MorningWarmups.java
+++ src/MorningWarmups.java
@@ -26,9 +26,8 @@ public static void main(String[] args) {
                 map.put(target-nums[i], i);
             }
         }
-        for (int i = 0; i < ; i++) {
-            
-        }
+
+
         return new int[]{0,0};
     }
 //
@@ -117,6 +116,28 @@ public static void main(String[] args) {
 //        return dummyHead.next;
 //    }
 //    }
+class Solution {
+    public boolean checkStraightLine(int[][] coordinates) {
+        int[] point1 = coordinates[1];
+        int[] point2 = coordinates[0];
+        float gSlope = slope(point1, point2);
+
+        for (int i = 1; i < coordinates.length; i++) {
+            int[] p1 = coordinates[i];
+            int[] p2 = coordinates[0];
+            float slop = slope(p1, p2);
+            if (gSlope != slop) return false;
+        }
+        return true;
+    }
+
+    private float slope(int[] p1, int[] p2) {
+        if ((p1[0] - p2[0]) == 0) return 0;
+        return (float)(p2[1] - p1[1]) / (p2[0] - p1[0]);
+    }
+}
+
+
 }
 //learn about Listnodes
 
