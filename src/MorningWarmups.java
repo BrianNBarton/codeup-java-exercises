a217878cb1f3cd77fe77468059830c1a5a87137d
Brian Barton <brian.n.barton@gmail.com>
2020-09-18 10:12:34 -0600

    remove Duplicates

--- src/MorningWarmups.java
+++ src/MorningWarmups.java
@@ -138,7 +138,33 @@ class Solution {
 }
 
 
-}
+        public int removeDuplicates(int[] nums) {
+
+            int length = nums.length;
+
+            if(length == 0)
+                return 0;
+
+            int i;
+            int targetIndex = 1;
+            int OG = nums[0];
+
+            for(i = 1; i < length; i++){
+
+                if(nums[i] != OG){
+
+                    OG = nums[i];
+                    nums[targetIndex] = nums[i];
+                    targetIndex++;
+                }
+            }
+
+            return targetIndex;
+
+        }
+    }
+
+
 //learn about Listnodes
 
 
