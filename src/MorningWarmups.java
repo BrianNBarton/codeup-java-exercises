08921bca658223bfd9026ff5dd46ecc48440495f
Brian Barton <brian.n.barton@gmail.com>
2020-09-19 16:57:51 -0600

    interesting boolean  methods

--- src/MorningWarmups.java
+++ src/MorningWarmups.java
@@ -162,9 +162,32 @@ class Solution {
             return targetIndex;
 
         }
+    //if perfect square, return true, else false
+    public boolean isPerfectSquare(int num) {
+        double s = Math.sqrt(num);
+        return s == (int)s;
     }
 
 
+    //if palendrone, return true
+    public boolean isPalindromeRange(String s, int i, int j) {
+        for (int k = i; k <= i + (j - i) / 2; k++) {
+            if (s.charAt(k) != s.charAt(j - k + i)) return false;
+        }
+        return true;
+    }
+    public boolean validPalindrome(String s) {
+        for (int i = 0; i < s.length() / 2; i++) {
+            if (s.charAt(i) != s.charAt(s.length() - 1 - i)) {
+                int j = s.length() - 1 - i;
+                return (isPalindromeRange(s, i+1, j) ||
+                        isPalindromeRange(s, i, j-1));
+            }
+        }
+        return true;
+    }
+}
+}
 //learn about Listnodes
 
 
