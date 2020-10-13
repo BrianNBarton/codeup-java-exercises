b2c896fda650eb98779c9273ca2fc2182a769749
Brian Barton <brian.n.barton@gmail.com>
2020-10-13 07:54:15 -0600

    string matching

--- src/LeetCode2.java
+++ src/LeetCode2.java
@@ -1,6 +1,8 @@
 import org.w3c.dom.ls.LSOutput;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class LeetCode2 {
@@ -102,6 +104,27 @@ public class LeetCode2 {
             product *= digit;
         }
         return product - sum;
+    }
+    public List<String> stringMatching(String[] words) {
+        HashSet<String> set = new HashSet<>();
+
+        for(int i = 0; i<words.length-1; i++) {
+            String currWord = words[i];
+            for(int j = i+1; j<words.length; j++) {
+                String nextWord = words[j];
+
+                if(currWord.contains(nextWord)) {
+                    set.add(nextWord);
+                }
+
+                if(nextWord.contains(currWord)) {
+                    set.add(currWord);
+                }
+            }
+        }
+
+        List<String> res = new ArrayList<String>(set); // convert hashset to list
+        return res;
     }
 
 }
