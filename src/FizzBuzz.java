601d1421c0f2522a05bb31a6130584416e92e73b
Brian Barton <brian.n.barton@gmail.com>
2020-11-01 19:07:42 -0700

    4 sum

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -2,11 +2,11 @@ public class FizzBuzz {
     public static void fizzBuzz(int num) {
         for (int i = 1; i < num; i++) {
             if (i % 3 == 0 && i % 5 == 0) {
-                System.out.printf("\n%d: fizz buzz", i);
+                System.out.printf("fizz buzz", i);
             } else if (i % 5 == 0) {
-                System.out.printf("\n%d: buzz", i);
+                System.out.printf("buzz", i);
             } else if (i % 3 == 0) {
-                System.out.printf("\n%d: fizz", i);
+                System.out.printf("fizz", i);
             } else {
                 System.out.printf("\n",i);
             }
--- src/LeetCode2.java
+++ src/LeetCode2.java
@@ -1,9 +1,6 @@
 import org.w3c.dom.ls.LSOutput;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class LeetCode2 {
     public static void main(String[] args) {
@@ -130,6 +127,42 @@ public class LeetCode2 {
         int res = 0, count[] = new int[101];
         for (int a: A) {
             res += count[a]++;
+        }
+        return res;
+    }
+
+
+
+
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        Arrays.sort(nums);
+        return kSum(nums, target, 0, 4);
+    }
+    public List<List<Integer>> kSum(int[] nums, int target, int start, int k) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (start == nums.length || nums[start] * k > target || target > nums[nums.length - 1] * k)
+            return res;
+        if (k == 2)
+            return twoSum(nums, target, start);
+        for (int i = start; i < nums.length; ++i)
+            if (i == start || nums[i - 1] != nums[i])
+                for (var set : kSum(nums, target - nums[i], i + 1, k - 1)) {
+                    res.add(new ArrayList<>(Arrays.asList(nums[i])));
+                    res.get(res.size() - 1).addAll(set);
+                }
+        return res;
+    }
+    public List<List<Integer>> twoSum(int[] nums, int target, int start) {
+        List<List<Integer>> res = new ArrayList<>();
+        int lo = start, hi = nums.length - 1;
+        while (lo < hi) {
+            int sum = nums[lo] + nums[hi];
+            if (sum < target || (lo > start && nums[lo] == nums[lo - 1]))
+                ++lo;
+            else if (sum > target || (hi < nums.length - 1 && nums[hi] == nums[hi + 1]))
+                --hi;
+            else
+                res.add(Arrays.asList(nums[lo++], nums[hi--]));
         }
         return res;
     }
